d7e5557b9194cce0a295655f830d47302ffded77
kissinger69 <kissyoa69@gmail.com>
2020-04-10 09:34:26 +0300

    script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $(".click0").click(function() {
+   $(".design0").slideToggle("slow");
+     $(".display0").slideToggle("slow");
+  });
+});
+$(".click1").click(function() {
+    $(".development1").slideToggle("slow");
+     $(".display1").slideToggle("show");
+});
+$(".click2").click(function()  {
+    $(".product2").slideToggle("slow");
+     $(".display2").slideToggle("slow");
+});
+
+
+
