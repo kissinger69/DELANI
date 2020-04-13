71e6f6c782ed6929ca704df42ffefb6a3e3288cf
kissinger69 <kissyoa69@gmail.com>
2020-04-13 14:43:53 +0300

    added hover to jQuery

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $(".click0").click(function() {
-   $(".design0").slideToggle("slow");
-     $(".display0").slideToggle("slow");
-  });
+    $(".design0").slideToggle("slow");
+      $(".display0").slideToggle("slow");
+   });  
 });
 $(".click1").click(function() {
     $(".development1").slideToggle("slow");
@@ -15,7 +15,45 @@ $(".click2").click(function()  {
 $(".path").hide();
 $(".squares").hover(function() {
   $(this).find(".path").toggle("slow");
+  $(this).find(".path").toggle("slow");
  });
+ $(".path1").hide();
+ $(".squares").hover(function() {
+   $(this).find(".path1").toggle("slow");
+   $(this).find(".path1").toggle("slow");
+  });
+  $(".path2").hide();
+ $(".squares").hover(function() {
+   $(this).find(".path2").toggle("slow");
+   $(this).find(".path2").toggle("slow");
+  });
+  $(".path3").hide();
+  $(".squares").hover(function() {
+    $(this).find(".path3").toggle("slow");
+    $(this).find(".path3").toggle("slow");
+   });
+   $(".path4").hide();
+   $(".squares").hover(function() {
+     $(this).find(".path4").toggle("slow");
+     $(this).find(".path4").toggle("slow");
+    });
+    $(".path5").hide();
+    $(".squares").hover(function() {
+      $(this).find(".path5").toggle("slow");
+      $(this).find(".path5").toggle("slow");
+     });
+     $(".path6").hide();
+     $(".squares").hover(function() {
+       $(this).find(".path6").toggle("slow");
+       $(this).find(".path6").toggle("slow");
+      });
+      $(".path7").hide();
+      $(".squares").hover(function() {
+        $(this).find(".path7").toggle("slow");
+        $(this).find(".path7").toggle("slow");
+       });
+
+
 
  
 
